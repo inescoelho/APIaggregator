7394cd1c278f08b9af4ff1dd4726f493c30956ab
Ines <ines.opcoelho@gmail.com>
2017-11-15 00:44:04 +0000

    display NASA search history on page

--- js/nasa.js
+++ js/nasa.js
@@ -1,24 +1,34 @@
+updateHistory();
+
 document.getElementById('search').getElementsByTagName('button')[0].addEventListener('click', function() {
-	getImages();
+	processInput(getInput());
 });
 
 document.getElementById('search').getElementsByTagName('input')[0].addEventListener('keypress', function(e) {
 	if(e.keyCode === 13) {
-		getImages();
+		processInput(getInput());
 	}
 })
 
-function getImages() {
-	addLoading();
+function processInput(input) {
+	getImages(input);
+	addSearchtoStorage(input);
+	updateHistory();
+}
 
+function getInput() {
 	//get user input or set default input
 	let userInput = document.getElementById('search').getElementsByTagName('input')[0].value;
-	let input = (userInput !== "") ? userInput : "moon";
+	return (userInput !== "") ? userInput : "moon";
+}
+
+function getImages(input) {
+	addLoading();
 
 	//set title
 	var title = document.getElementById('result').getElementsByTagName('h2')[0];
 	title.innerHTML = "";
-	title.innerHTML = `Image results for ${input}`;
+	title.innerHTML = `Results for ${input}`;
 
 	fetch("https://images-api.nasa.gov/search?q=" + input.replace(" ", "+"))
 		.then(function(r) {
@@ -38,8 +48,6 @@ function getImages() {
 		function (err) {
 			processNoResults();
     };
-
-    addSearchtoStorage(input);
 }
 
 function processNoResults() {
@@ -50,20 +58,26 @@ function processNoResults() {
 
 function addResults(results) {
 	var div = document.getElementById('resultList');
+	//clean previous results
 	div.innerHTML = "";
-	var list = document.createElement('ul');
-	list.setAttribute("id", "listImages");
-	
 
 	results.forEach(function(r) {
-		var li = document.createElement('li');
+		//get data
 		let href = r.links[0].href;
 		let desc = r.data[0].description;
-		li.innerHTML = `<a href="${href}"><img src="${href}" alt="${desc}" height='80' width='80'></a>`;
-		list.appendChild(li);
-	});
 
-	div.appendChild(list);
+		//create div
+		let el = document.createElement('div');
+		el.className += "col-md-4 col-sm-6";
+		//create anchor element
+		let anchor = document.createElement('a');
+		anchor.href = `${href}`;
+		anchor.className += "d-block mb-4 h-100";
+		anchor.innerHTML = `<img src="${href}" class="img-fluid img-thumbnail" width='100%' data-toggle="tooltip" title="${desc}">`;
+		//add elements to list
+		el.appendChild(anchor);
+		div.appendChild(el);
+	});
 }
 
 function addLoading() {
@@ -72,13 +86,41 @@ function addLoading() {
 	text.append("Loading...");
 }
 
-function addSearchtoStorage(input) {
-	var searchHistory = localStorage.getItem("searchHistory");
-	if(!searchHistory) {
-		searchHistory = "";
+function getHistory() {
+	var historyObj = { searches: [] };
+	if(localStorage.getItem('history') != null) {
+	    historyObj = JSON.parse(localStorage.getItem('history'));
 	}
+	return historyObj;
+}
 
-	searchHistory += "," + input;
-	localStorage.setItem('searchHistory', searchHistory);
-	console.log(searchHistory);
+function addSearchtoStorage(dataToSave) {
+	var historyObj = getHistory();
+	var index = historyObj.searches.indexOf(dataToSave);
+	
+	if (historyObj.searches.indexOf(dataToSave) !=-1) {
+	 	historyObj.searches.splice(index, 1);
+
+	}
+	historyObj.searches.push(dataToSave);
+
+	//keep only 10 elements in memory
+	if (historyObj.searches.length > 10) {
+    	historyObj.searches.splice(0, 1);
+	}
+	localStorage.setItem('history',JSON.stringify(historyObj));
+}
+
+function updateHistory() {
+	var history = getHistory();
+
+	if (history.searches.length > 0) {
+		var div = document.getElementById("history");
+		div.innerHTML = "<h4>Past searches: "
+
+		for (var i=0; i < history.searches.length; i++) 
+			div.innerHTML += `<button type="button" class="btn btn-link" onclick="processInput('${history.searches[i]}');return false;">${history.searches[i]} </button>`;
+
+		div.innerHTML += "</h4>"
+	}
 }
